0b9d332fcef9efffd646a3ce8fee0bd65581169a
Hinn <64025546+TheHinn@users.noreply.github.com>
2021-10-08 15:36:33 +0200

    FINISHED UGH

--- Exc 11.py
+++ Exc 11.py
@@ -1,4 +1,11 @@
 from RobotArm import RobotArm
-robotArm = RobotArm('exersice 11')
-for i in range(20):
-    robotArm.moveRight()
+robotArm = RobotArm('exercise 11')
+robotArm.speed = 2
+# Jouw python instructies zet je vanaf hier:
+robotArm.grab()
+print(robotArm.scan())
+for i in range(8):
+    if robotArm.scan() == "white":
+        robotArm.moveRight(), robotArm.drop(), robotArm.moveRight(), robotArm.grab(), print(robotArm.scan())
+    else:
+        robotArm.drop(), robotArm.moveRight(), robotArm.grab(), print(robotArm.scan())
--- Exc 6.py
+++ Exc 6.py
@@ -5,12 +5,6 @@ robotArm.speed = 3
 # Jouw python instructies zet je vanaf hier:
 for i in range(7):
     robotArm.moveRight()
-robotArm.moveRight()
-robotArm.moveRight()
-robotArm.moveRight()
-robotArm.moveRight()
-robotArm.moveRight()
-robotArm.moveRight()
 robotArm.grab()
 robotArm.moveRight()
 robotArm.drop()
